80aa6a7cd32dd9214d4c87953773fd9643c9d69c
Pham Duy Hoang <97928971+kng-duyhoang@users.noreply.github.com>
2026-02-03 16:19:08 +0700

    update component

--- apps/admin-dashboard/src/App.tsx
+++ apps/admin-dashboard/src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
-
+import { Button } from "@repo/ui"
 function App() {
 
   const [isDark, setIsDark] = useState(false);
 
   return (
     <>
-    <div className="p-10 space-y-10 bg-gray-200 min-h-screen" data-theme={isDark ? 'dark' : 'light'}>
-      <button onClick={() => setIsDark(!isDark)}>changeMode</button>
+    <div className="p-10 space-y-10 min-h-screen" data-theme={isDark ? 'dark' : 'light'}>
+      <Button className="me-2" color="secondary" size="default" variant={"default"}>Default</Button>
+      <Button size="sm" variant={"secondary"}>Large</Button>
+      <div className="bg-primary p-10 text-white">Test Tailwind</div>
+      <Button onClick={() => setIsDark(!isDark)}>changeMode</Button>
       <div className="p-6 bg-background text-foreground border rounded-xl shadow-md">
         <p className="text-red">Đây là màu Primary mặc định</p>
         <h2 className="font-bold text-primary">LIGHT MODE</h2>
